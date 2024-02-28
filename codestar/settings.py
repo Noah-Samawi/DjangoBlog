b6d00b73b8d2694d905a56ee5eb94262bda28cec
Noah Samawi <noah.alsamawi@gmail.com>
2024-02-28 11:34:15 +0100

    fixe user commet issus

--- codestar/settings.py
+++ codestar/settings.py
@@ -172,7 +172,11 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
- 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
