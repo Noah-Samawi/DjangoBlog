29d8e8f183334b2d6d1987773b812cf1f0f46fe7
Noah Samawi <noah.alsamawi@gmail.com>
2024-02-28 16:39:22 +0100

    Update settings.py for med final deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -55,8 +55,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost" '127.0.0.1', 'django-blog-pp-e9fa78e1f8b2.herokuapp.com' ]
-
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
